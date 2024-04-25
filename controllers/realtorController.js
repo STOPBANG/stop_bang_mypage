6bb964d58f38eee3cf0c8d84aa03590cc34ebbe9
ywonchae1 <ywonchae62@gmail.com>
2024-04-25 13:07:45 +0900

    feat: 부동산 홈페이지가 DB와 통신하도록 설정

--- controllers/realtorController.js
+++ controllers/realtorController.js
@@ -3,13 +3,13 @@ const e = require("express");
 const realtorModel = require("../models/realtorModel.js");
 const tags = require("../public/assets/tag.js");
 const jwt = require("jsonwebtoken");
-
+const { httpRequest } = require('../utils/httpRequest.js');
 
 const makeStatistics = (reviews) => {
   let array = Array.from({ length: 10 }, () => 0);
   let stArray = new Array(10);
   reviews.forEach((review) => {
-    if(review.tags !== null) {
+    if (review.tags !== null) {
       review.tags.split("").forEach((tag) => {
         array[parseInt(tag)]++;
       });
@@ -28,62 +28,107 @@ module.exports = {
 
   mainPage: async (req, res) => {
     //쿠키로부터 로그인 계정 알아오기
-    let r_username = res.locals.r_username;
-    console.log(r_username);
-    try {
-      let who = await realtorModel.whoAreYou(r_username);
-      let agent = await realtorModel.getRealtorPublicData(req.params.ra_regno);
-      let agentPrivate = await realtorModel.getRealtorPrivateData(req.params.ra_regno);
-      let getReviews = await realtorModel.getReviewByRaRegno(
-        req.params.ra_regno,
-        r_username
+    if (req.cookies.authToken == undefined)
+      res.render("notFound.ejs", { message: "로그인이 필요합니다" });
+    else {
+      const decoded = jwt.verify(
+        req.cookies.authToken,
+        process.env.JWT_SECRET_KEY
       );
-      let getMyReport = await realtorModel.getReport(req.params, r_username);
-      let statistics;
-      if(getReviews.length > 0)
-        statistics = makeStatistics(getReviews);
-      let getRating = await realtorModel.getRating(req.params);
-      getRating = getRating === null ? 0 : getRating;
+      let r_username = decoded.userId;
+      if (r_username === null)
+        res.render("notFound.ejs", { message: "로그인이 필요합니다" });
 
-      res.locals.who = who;
-      res.locals.agent = agent[0];
-      res.locals.agentPrivate = agentPrivate.length ? agentPrivate[0] : null;
-      res.locals.agentReviewData = getReviews;
-      res.locals.report = getMyReport;
-      res.locals.statistics = statistics;
-      res.locals.rating = getRating;
+      try {
+        const response = {};
 
-      res.locals.title = `${res.locals.agent.cmp_nm}의 후기`;
-      res.locals.direction = `/review/${req.params.ra_regno}/create`;
-      res.locals.cmpName = res.locals.agent.cmp_nm;
+        response.r_username = r_username;
 
-      res.locals.openedReviewData = null;
-      res.locals.canOpen = null;
-      res.locals.bookmark = 0;
+        // [start] 로그인 계정 정보 가져오기
+        response.who = req.cookies.userType;
+        // [end] 로그인 계정 정보 가져오기
 
-      if(who === 1) {
-        let getOpened = await realtorModel.getOpenedReview(r_username);
-        let canOpen = await realtorModel.canIOpen(r_username);
-        let getBookmark = await realtorModel.getBookmarkByIdnRegno(
-          req.params.ra_regno,
-          r_username
-        );
-        res.json({
-          openedReviewData: getOpened,
-          canOpen: canOpen,
-          bookmark: getBookmark[0][0] ? getBookmark[0][0] : 0,
-          path: 'realtor'
-        })
+        // [start] 공인중개사 공공데이터 가져오기
+        const getOptions1 = {
+          host: 'stop_bang_auth_DB',
+          port: process.env.PORT,
+          path: `/db/agentlist/findById/${req.params.ra_regno}`,
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }
+
+        result = await httpRequest(getOptions1)
+        if (result.body.length)
+          response.agent = result.body[0];
+        else
+          response.agent = null;
+        // [end] 공인중개사 공공데이터 가져오기
+
+        // [start] 공인중개사 개인정보 가져오기
+        postOptions = {
+          host: 'stop_bang_auth_DB',
+          port: process.env.PORT,
+          path: `/db/agent/findByRaRegno/${req.params.ra_regno}`,
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }
+        requestBody = { username: r_username };
+
+        result = await httpRequest(postOptions, requestBody);
+        if (result.body.length)
+          response.agentPrivate = result.body[0];
+        else
+          response.agentPrivate = null;
+        // [end] 공인중개사 개인정보 가져오기
+
+        // 이부분도 DB 서버와 통신하는 것으로 변경해야 합니다
+        response.rating = 0;
+        response.review = [];
+        response.agentReviewData = [];
+        response.statistics = [];
+        response.report = null;
+        response.openedReviewData = null;
+        response.canOpen = null;
+        response.tagsData = null;
+        response.bookmark = 0;
+        response.direction = '';
+
+        console.log(response);
+        return res.json(response);
+
+        // let getMyReport = await realtorModel.getReport(req.params, r_username);
+        // let statistics;
+        // if(getReviews.length > 0)
+        //   statistics = makeStatistics(getReviews);
+        // let getRating = await realtorModel.getRating(req.params);
+        // getRating = getRating === null ? 0 : getRating;
+
+        // if(who === 1) {
+        //   let getOpened = await realtorModel.getOpenedReview(r_username);
+        //   let canOpen = await realtorModel.canIOpen(r_username);
+        //   let getBookmark = await realtorModel.getBookmarkByIdnRegno(
+        //     req.params.ra_regno,
+        //     r_username
+        //   );
+
+        //   response.openedReviewData = getOpened;
+        //   response.canOpen = canOpen;
+        //   response.bookmark = getBookmark[0][0] ? getBookmark[0][0] : 0;
+        // }
+
+        // if (getRating === null) {
+        //   response.tagsData = null;
+        // } else {
+        //   response.tagsData = tags.tags;
+        // }
+      } catch (err) {
+        console.error(err.stack);
+        return res.json({});
       }
-
-      if (getRating === null) {
-        res.locals.tagsData = null;
-      } else {
-        res.locals.tagsData = tags.tags;
-      }
-      res.json += ({tagsData: getRating ? tags.tags : null})
-    } catch (err) {
-      console.error(err.stack);
     }
-  }
+  },
 };
