db8a80d3854c1f18ac77f675c08258c7d7967e58
ywonchae1 <ywonchae62@gmail.com>
2024-04-25 16:59:04 +0900

    #3 feat: 공인중개사 공공데이터 open api로 변경 및 부동산 홈페이지 후기 가져오는 통신 구현

--- controllers/realtorController.js
+++ controllers/realtorController.js
@@ -49,21 +49,14 @@ module.exports = {
         // [end] 로그인 계정 정보 가져오기
 
         // [start] 공인중개사 공공데이터 가져오기
-        const getOptions1 = {
-          host: 'stop_bang_auth_DB',
-          port: process.env.PORT,
-          path: `/db/agentlist/findById/${req.params.ra_regno}`,
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          }
-        }
+        // 서울시 공공데이터 api
+        const apiResponse = await fetch(
+          `http://openapi.seoul.go.kr:8088/${process.env.API_KEY}/json/landBizInfo/1/1/${req.params.ra_regno}`
+        );
+        const js = await apiResponse.json();
+        const agentPublicData = js.landBizInfo.row[0];
 
-        result = await httpRequest(getOptions1)
-        if (result.body.length)
-          response.agent = result.body[0];
-        else
-          response.agent = null;
+        response.agent = agentPublicData;
         // [end] 공인중개사 공공데이터 가져오기
 
         // [start] 공인중개사 개인정보 가져오기
@@ -85,9 +78,25 @@ module.exports = {
           response.agentPrivate = null;
         // [end] 공인중개사 개인정보 가져오기
 
+        // [start] 부동산 후기 가져오기
+        getOptions = {
+          host: 'stop_bang_review_DB',
+          port: process.env.PORT,
+          path: `/db/review/findAllByRegno/${req.params.ra_regno}`,
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }
+        result = await httpRequest(getOptions);
+        if (result.body.length)
+          response.review = result.body;
+        else
+          response.review = null;
+        // [end] 부동산 후기 가져오기
+
         // 이부분도 DB 서버와 통신하는 것으로 변경해야 합니다
         response.rating = 0;
-        response.review = [];
         response.agentReviewData = [];
         response.statistics = [];
         response.report = null;
