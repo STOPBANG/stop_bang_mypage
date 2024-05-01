2fa1daa469701220ddd8595036136778e7472884
plum-king <jeesoo0908@gmail.com>
2024-05-01 23:32:59 +0900

    fix: 임시 주석 처리

--- routers/agentRouter.js
+++ routers/agentRouter.js
@@ -13,16 +13,16 @@ router.get("/:id", agentController.agentProfile);
 
 router.get("/:id/update", agentController.updateEnteredInfo);
 
-router.post(
-    "/:id/update_process",
-    agentController.upload.single("myImage"),
-    agentController.updatingEnteredInfo,
-);
+// router.post(
+//     "/:id/update_process",
+//     agentController.upload.single("myImage"),
+//     agentController.updatingEnteredInfo,
+// );
 
 router.get("/:id/info_edit", agentController.updateMainInfo);
 
-router.post(
-    "/:id/edit_process",
-    agentController.upload.fields([{name: 'myImage1'}, {name: 'myImage2'}, {name: 'myImage3'}]),
-    agentController.updatingMainInfo,
-);
+// router.post(
+//     "/:id/edit_process",
+//     agentController.upload.fields([{name: 'myImage1'}, {name: 'myImage2'}, {name: 'myImage3'}]),
+//     agentController.updatingMainInfo,
+// );
