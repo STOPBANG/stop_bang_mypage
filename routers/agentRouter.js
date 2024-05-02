9b1c60901c37f96c3649bcd75605e81baef30ff0
yewonahn <yewonahn@naver.com>
2024-05-02 23:57:17 +0900

    feat : 영업시간, 전화번호 수정 기능 경로

--- routers/agentRouter.js
+++ routers/agentRouter.js
@@ -11,20 +11,21 @@ router.use((req, res, next) => {
 
 router.get("/:id", agentController.agentProfile);
 
-router.get("/:id/update", agentController.updateEnteredInfo);
-
-// router.post(
-//     "/:id/update_process",
-//     agentController.upload.single("myImage"),
-//     agentController.updatingEnteredInfo,
-// );
-
 router.get("/:id/info_edit", agentController.updateMainInfo);
 
 // router.post(
 //     "/:id/edit_process",
 //     agentController.upload.fields([{name: 'myImage1'}, {name: 'myImage2'}, {name: 'myImage3'}]),
 //     agentController.updatingMainInfo,
+// );
+
+router.get("/:id/entered_info_process", agentController.updateEnteredInfo);
+
+//  부동산 홈페이지 영업시간, 전화번호 수정 사항 저장
+// router.post(
+//     "/:id/entered_info_update",
+//     agentController.upload.single("myImage"),
+//     agentController.updatingEnteredInfo,
 // );
 
 module.exports = router;
