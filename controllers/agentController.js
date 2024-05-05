26eb40d1a1ae6b27b8639ae0bca63dea4b8c5006
ywonchae1 <ywonchae62@gmail.com>
2024-05-05 18:08:46 +0900

    #5 feat: url sys_regno를 가지고 공공데이터 조회, 회원 정보 조회 구현

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -17,91 +17,93 @@ const storage = new Storage({
 });
 const bucket = storage.bucket(GCP_BUCKET_NAME);
 
+function jsonKeyLowerCase(object){
+    if(Array.isArray(object)){
+    // 리스트<맵> 형식으로 넘어오는 경우 처리
+        object.forEach((item, index) =>{
+            object[index] = Object.fromEntries(Object.entries(item).map(([key, value]) => [key.toLowerCase(), value]));
+        });
+        return object;
+    }
+    else {
+        // 맵 형식으로 넘어오는 경우 처리
+        return Object.fromEntries(Object.entries(object).map(([key, value]) => [key.toLowerCase(), value]));
+    }
+}
+
 module.exports = {
     agentProfile: async (req, res, next) => {
+        const ra_regno = req.params.ra_regno;
+        const response = {};
         try {
-            const response = {};
-            // [start] agent 프로필 정보 가져오기
             const getProfileOptions = {
                 host: 'stop_bang_auth_DB',
-                port: process.env.MS_PORT,
-                path: `/db/agent/findByRaRegno/${req.params.ra_regno}`,
+                port: process.env.PORT,
+                path: `/db/agent/findByRaRegno/${ra_regno}`,
                 method: 'GET',
                 headers: {
                   'Content-Type': 'application/json',
                 }
               }
               httpRequest(getProfileOptions)
-              .then((profileRes) => {
-                const apiResponse = fetch(
-                    `http://openapi.seoul.go.kr:8088/${process.env.API_KEY}/json/landBizInfo/1/1/${req.params.ra_regno}`
+              .then(async (profileRes) => {
+                const apiResponse = await fetch(
+                    `http://openapi.seoul.go.kr:8088/${process.env.API_KEY}/json/landBizInfo/1/1/${ra_regno}/`
                   );
-                const js = apiResponse.json();
+                const js = await apiResponse.json();
                 if (js.landBizInfo == undefined){
                     response.agent = null;
-                    // response.agentMainInfo = null;
+                    response.agentMainInfo = null;
+                    response.agentSubInfo = null;
                 }
                 else{
-                    response.agent = js.landBizInfo.row[0];
-                    // response.agentMainInfo = js.landBizInfo.row[0];
+                    const agentData = jsonKeyLowerCase(js.landBizInfo.row[0]);
+                    response.agent = agentData;
+                    response.agentMainInfo = profileRes.body[0];
+                    response.agentSubInfo = profileRes.body[0];
                 }
-                if(profileRes.body.length){
-                    response.agent += profileRes.body[0];
-                    // response.agentMainInfo += profileRes.body[0];
-                }
-                else{
-                    response.agent = null;
-                    // response.agentMainInfo = null;
-                }
-                console.log(response.agent);
-                // // [end] agent main info 가져오기 (생략해도 될듯)
-                // const getMainInfoOptions = {
-                //     host: 'stop_bang_auth_DB',
-                //     port: process.env.MS_PORT,
-                //     path: `/db/agent/findByRaRegno/${req.params.ra_regno}`,
-                //     method: 'GET',
-                //     headers: {
-                //       'Content-Type': 'application/json',
-                //     }
-                //   }
 
+                /* gcs */
+                const profileImage = profileRes.a_profile_image;
+                if(profileImage !== null) {
+                    response.a_profile_image = bucket.file(`agent/${profileImage}`).publicUrl();
+                }
+
+                response.agentRating = null;
+                response.agentReviewData = [];
+                response.report = null;
+                response.statistics = null;
+                response.tagsData = null;
+                return res.json(response);
               })
         // let agent = await agentModel.getAgentProfile(req.params.id);
         // let getMainInfo = await agentModel.getMainInfo(req.params.id);
         
 
-        if (getMainInfo.a_username !== decoded.userId) // main으로 통신 보낼 때 확인 
-            res.render('notFound.ejs', {message: "접근이 제한되었습니다. 공인중개사 계정으로 로그인하세요"});
+        // if (getMainInfo.a_username !== decoded.userId) // main으로 통신 보낼 때 확인 
+            // res.render('notFound.ejs', {message: "접근이 제한되었습니다. 공인중개사 계정으로 로그인하세요"});
         // let getEnteredAgent = await agentModel.getEnteredAgent(req.params.id);
-        let getReviews = await agentModel.getReviewByRaRegno(req.params.id);
-        let getReport = await agentModel.getReport(req.params.id, decoded.userId);
-        let getRating = await agentModel.getRating(req.params.id);
-        let statistics = makeStatistics(getReviews);
+        // let getReviews = await agentModel.getReviewByRaRegno(req.params.id);
+        // let getReport = await agentModel.getReport(req.params.id, decoded.userId);
+        // let getRating = await agentModel.getRating(req.params.id);
+        // let statistics = makeStatistics(getReviews);
         // res.locals.agent = agent[0];
         // res.locals.agentMainInfo = getMainInfo;
         // res.locals.agentSubInfo = getEnteredAgent[0][0];
-        res.locals.agentReviewData = getReviews;
-        res.locals.report = getReport;
-        res.locals.statistics = statistics;
+        // res.locals.agentReviewData = getReviews;
+        // res.locals.report = getReport;
+        // res.locals.statistics = statistics;
 
-        if (getRating === null) {
-            res.locals.agentRating = 0;
-            res.locals.tagsData = null;
-        } else {
-            res.locals.agentRating = getRating;
-            res.locals.tagsData = tags.tags;
-        }
-
-        /* gcs */
-        const profileImage = response.a_profile_image;
-        if(profileImage !== null) {
-            response.a_profile_image = bucket.file(`agent/${profileImage}`).publicUrl();
-            // res.locals.agent.a_profile_image = bucket.file(`agent/${profileImage}`).publicUrl();
-        }
+        // if (getRating === null) {
+        //     res.locals.agentRating = 0;
+        //     res.locals.tagsData = null;
+        // } else {
+        //     res.locals.agentRating = getRating;
+        //     res.locals.tagsData = tags.tags;
+        // }
         } catch (err) {
-        console.error(err.stack);
+            console.error(err.stack);
         }
-        next();
     },
 
     updateMainInfo: async (req, res) => {
