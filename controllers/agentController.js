3178c671cbc5f53598e96c576bf53ee6e4e67490
plum-king <jeesoo0908@gmail.com>
2024-05-15 17:44:33 +0900

    #8 feat: upload 추가

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -48,6 +48,14 @@ function jsonKeyLowerCase(object){
 }
 
 module.exports = {
+    upload: multer({
+        storage: multer.memoryStorage(),
+        limits: { fileSize: 10 * 1024 * 1024 },
+        fileFilter: function (req, file, cb) {
+          checkFileType(file, cb);
+        },
+      }),
+      
     agentProfile: async (req, res, next) => {
         const ra_regno = req.params.ra_regno;
         const response = {};
