19373efdcc49635d570119840547a7949e92a352
yewonahn <yewonahn@naver.com>
2024-05-16 20:29:12 +0900

    #8 fix : ra_regno 통일
    경로 수정

--- routers/agentRouter.js
+++ routers/agentRouter.js
@@ -19,13 +19,12 @@ router.post(
     agentController.updatingMainInfo,
 );
 
-router.get("/:id/entered_info_process", agentController.updateEnteredInfo);
+router.get("/:ra_regno/entered_info_process", agentController.updateEnteredInfo);
 
-//  부동산 홈페이지 영업시간, 전화번호 수정 사항 저장
-// router.post(
-//     "/:id/entered_info_update",
-//     agentController.upload.single("myImage"),
-//     agentController.updatingEnteredInfo,
-// );
+router.post(
+    "/:ra_regno/entered_info_update",
+    agentController.upload.single("myImage"),
+    agentController.updatingEnteredInfo,
+);
 
 module.exports = router;
